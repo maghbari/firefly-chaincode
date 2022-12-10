ca77c451bf9abc6681193a7b8aedebfdf3a3e8e5
mo <alaghbri.mohammed@gmail.com>
2022-12-10 23:41:32 +0300

    fix errors

--- contracts/firefly/main.go
+++ contracts/firefly/main.go
@@ -1,10 +1,11 @@
-package chaincode
+// SPDX-License-Identifier: Apache-2.0
+
+package main
 
 import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -12,6 +13,36 @@ import (
 // SmartContract provides functions for managing an Asset
 type SmartContract struct {
 	contractapi.Contract
+}
+
+// PutValue - Adds a key value pair to the world state
+func (sc *SmartContract) PutValue(ctx contractapi.TransactionContextInterface, key string, value string) error {
+	return ctx.GetStub().PutState(key, []byte(value))
+}
+
+// GetValue - Gets the value for a key from the world state
+func (sc *SmartContract) GetValue(ctx contractapi.TransactionContextInterface, key string) (string, error) {
+	bytes, err := ctx.GetStub().GetState(key)
+
+	if err != nil {
+		return "", nil
+	}
+
+	return string(bytes), nil
+}
+
+func main() {
+	SmartContract := new(SmartContract)
+
+	cc, err := contractapi.NewChaincode(SmartContract)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err := cc.Start(); err != nil {
+		panic(err.Error())
+	}
 }
 
 type BatchPinEvent struct {
--- firefly-go/firefly.go
+++ firefly-go/firefly.go
@@ -8,16 +8,16 @@ import (
 	"log"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	"github.com/hyperledger/firefly/chaincode-go/chaincode"
+	"github.com/hyperledger/maghbari/firefly-chaincode/contracts/firefly"
 )
 
 func main() {
-	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	fireflyChaincode, err := contractapi.NewChaincode(&firefly.SmartContract{})
 	if err != nil {
-		log.Panicf("Error creating asset_transfer_basic chaincode: %v", err)
+		log.Panicf("Error creating firefly chaincode: %v", err)
 	}
 
-	if err := assetChaincode.Start(); err != nil {
-		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
+	if err := fireflyChaincode.Start(); err != nil {
+		log.Panicf("Error starting afirefly chaincode: %v", err)
 	}
 }
